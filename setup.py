0cdcbb2f058f0ca3965637fb16e41429aa39d4d5
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2017-10-29 10:36:06 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 setup(
     name='poeditorcli',
     version='0.3',
-    version='0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
