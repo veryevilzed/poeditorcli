463506c11d2af8d81ed03b4781c89a7693ed3bc8
Igor "SkAZi" Potapov <igor@potapoff.org>
2022-02-16 13:35:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='poeditorcli',
-    version='0.12',
+    version='0.13',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
